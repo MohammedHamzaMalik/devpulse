d62041e08f86f4b625922d826d513c817231f1f3
Mohammed Hamza Malik <m.h.m.i.malik@gmail.com>
2026-03-31 18:05:36 -0500

    added the jwt security files

---
+++ api/devpulse-api/src/main/java/com/hamza/devpulseapi/security/JwtUtil.java
@@ -0,0 +1,44 @@
+package com.hamza.devpulseapi.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET = "thisIsASecretKeyThatIsLongEnoughForHS256Algorithm!";
+    private static final long EXPIRY = 1000 * 60 * 60 * 24;
+
+    private final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + EXPIRY))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            extractEmail(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
