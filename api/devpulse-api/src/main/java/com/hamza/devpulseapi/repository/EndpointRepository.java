06882c26a4bc0afe9c618b93b51ae9c104bcfbd5
Mohammed Hamza Malik <m.h.m.i.malik@gmail.com>
2026-04-02 21:08:09 -0500

    added a query for finding all active endpoints

--- api/devpulse-api/src/main/java/com/hamza/devpulseapi/repository/EndpointRepository.java
+++ api/devpulse-api/src/main/java/com/hamza/devpulseapi/repository/EndpointRepository.java
@@ -2,9 +2,13 @@ package com.hamza.devpulseapi.repository;
 
 import com.hamza.devpulseapi.model.Endpoint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface EndpointRepository extends JpaRepository<Endpoint, Long> {
     List<Endpoint> findByOwnerEmailAndActiveTrue(String ownerEmail);
+
+    @Query("SELECT e FROM Endpoint e WHERE e.active = true")
+    List<Endpoint> findAllActive();
 }
